f07f21e940a2f7948e6e4d189f455bee3958e9f2
Dickerson, Kevin Nole <kdickerson30@gatech.edu>
2020-12-29 22:14:36 -0500

    Updated site to be dark mode friendly

--- server.js
+++ server.js
@@ -3,32 +3,32 @@ let axios = require('axios');
 let http = require('http'); // Import Node.js core module
 let fs = require('fs');
 
-const test2 = {
-    steamID: "76561198045936277",
-    username: "RNvK",
-    gameID: "1343400",
-    achievementName: "Who's the Boss?",
-    avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/04/04e34a428ad7f5f3168c60174c8cecec5f11f809_medium.jpg",
-};
 const kevin = {
     steamID: "76561198045936277",
-    username: "Pulkit",
-    gameID: "413150",
-    achievementName: "Local Legend",
+    username: "Kevin",
+    gameID: "378860",
+    achievementName: "Manual All The Way",
+    avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/04/04e34a428ad7f5f3168c60174c8cecec5f11f809_medium.jpg",
+};
+const kevin2 = {
+    steamID: "76561198045936277",
+    username: "Kevin",
+    gameID: "378860",
+    achievementName: "No Man's Fly",
     avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/04/04e34a428ad7f5f3168c60174c8cecec5f11f809_medium.jpg",
 };
 const pulkit = {
     steamID: "76561198245431724",
     username: "Pulkit",
-    gameID: "Sanctum2",
-    achievementName: "Growing Stronger",
+    gameID: "413150",
+    achievementName: "Local Legend",
     avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/90/904077ede20da3e813726acbebb50d60049ba360_medium.jpg",
 };
 const aj = {
     steamID: "76561198253719597",
     username: "AJ",
-    gameID: "244850",
-    achievementName: "Millionaire Club",
+    gameID: "648350",
+    achievementName: "Veggiesaurus",
     avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg",
 };
 
@@ -45,8 +45,6 @@ let server = http.createServer(function (req, res) {   //create web server
 	<title>Steam A.S.S.</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
@@ -80,15 +78,15 @@ let server = http.createServer(function (req, res) {   //create web server
         }
         
         let tableEntry = extractAchievementInfo(kevin);
-        let tableEntry2 = extractAchievementInfo(test2);
-        let tableEntry3 = extractAchievementInfo(pulkit);
-        let tableEntry4 = extractAchievementInfo(aj)
-        Promise.all([tableEntry, tableEntry2, tableEntry3, tableEntry4])
+        let tableEntry2 = extractAchievementInfo(pulkit);
+        let tableEntry3 = extractAchievementInfo(aj)
+        let tableEntry4 = extractAchievementInfo(kevin2);
+        Promise.all([tableEntry2, tableEntry3, tableEntry, tableEntry4])
             .then((entries) => {
                 entries.forEach((entry) => {
                     if(entry.complete == 1){
                         res.write(`
-                            <tr class="complete">
+                            <tr class="complete" onClick="document.location.href='https://steamcommunity.com/profiles/${entry.steamID}/stats/${entry.gameID}'">
                                 <td class="column1">
                                     <br>
                                     <img src="${entry.avatar}" alt="" />
@@ -112,7 +110,7 @@ let server = http.createServer(function (req, res) {   //create web server
                         `);
                     } else {
                         res.write(`
-                            <tr>
+                            <tr onClick="document.location.href='https://steamcommunity.com/profiles/${entry.steamID}/stats/${entry.gameID}'">
                                 <td class="column1">
                                     <br>
                                     <img src="${entry.avatar}" alt="" />
@@ -162,6 +160,18 @@ let server = http.createServer(function (req, res) {   //create web server
         let fileStream = fs.createReadStream("./vendor/select2/select2.min.css");
         res.writeHead(200, { 'Content-Type': 'text/css' });
         fileStream.pipe(res)
+    } else if (req.url == '/fonts/OpenSans/OpenSans-Regular.ttf') {
+        let fileStream = fs.createReadStream("./fonts/OpenSans/OpenSans-Regular.ttf");
+        res.writeHead(200, { 'Content-Type': 'font/ttf' });
+        fileStream.pipe(res)
+    } else if (req.url == '/images/icons/favicon.ico') {
+        let fileStream = fs.createReadStream("./images/icons/favicon.ico");
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        fileStream.pipe(res)
+    } else if (req.url == '/favicon.ico') {
+        let fileStream = fs.createReadStream("./images/icons/favicon.ico");
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+        fileStream.pipe(res)
     }
     else
         res.end(`Invalid Request to ${req.url}`);
@@ -170,7 +180,7 @@ let server = http.createServer(function (req, res) {   //create web server
 
 server.listen(8080); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+console.log('Node.js web server at port 8080 is running..')
 
 async function extractAchievementInfo(input){
     let response = await axios.get(`https://steamcommunity.com/profiles/${input.steamID}/stats/${input.gameID}/?xml=1`);
@@ -192,6 +202,8 @@ async function extractAchievementInfo(input){
             gameName: "",
             username: "",
             avatar: "",
+            steamID: "",
+            gameID: ""
         }
         
         achievements.forEach(achievement => {
@@ -208,6 +220,8 @@ async function extractAchievementInfo(input){
                 out.gameName = steamProfile.elements[0].elements[2].elements[1].elements[0].cdata;
                 out.username = input.username;
                 out.avatar = input.avatar;
+                out.steamID = input.steamID;
+                out.gameID = input.gameID;
             }
         });
         return out;
