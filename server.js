3ab2b5656cf39965453afaec30f287b4d58569b3
Kevin Dickerson <nolekev1214@gmail.com>
2023-12-15 17:08:58 -0500

    remove pointless else-if

--- server.js
+++ server.js
@@ -7,29 +7,25 @@ let members = require("./members.json");
 
 let server = http.createServer(function (req, res) {
     if (req.url == '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
         let promiseTable = [];
-        members.forEach(function(member) {
+        members.forEach(function (member) {
             promiseTable.push(extractAchievementInfo(member));
         });
 
         Promise.all(promiseTable)
             .then((entries) => {
                 entries = entries.filter(item => item.gameIcon !== "");
-                let out = nunjucks.render('templates/main.njk', {gamers: entries});
+                let out = nunjucks.render('templates/main.njk', { gamers: entries });
                 res.write(out);
                 res.end();
             })
             .catch(error => console.log(error.message));
     }
-    else if(req.url == '/2021'){
-        res.writeHead(302, { location: "/static/2021.html" });
-        res.end();
-    }
-    else{
-        fs.readFile(__dirname + req.url, function(err, data){
-            if(err){
+    else {
+        fs.readFile(__dirname + req.url, function (err, data) {
+            if (err) {
                 res.writeHead(404);
                 res.end(`Invalid Request to ${req.url}`);
             }
@@ -43,49 +39,49 @@ let server = http.createServer(function (req, res) {
 server.listen(8080); //Run Locally
 //console.log('Node.js web server at port 8080 is running..');
 
-async function extractAchievementInfo(input){
+async function extractAchievementInfo(input) {
     let response = await axios.get(`https://steamcommunity.com/profiles/${input.steamID}/stats/${input.gameID}/?xml=1`);
     let out = {
-            complete: 0,
-            iconComplete: "",
-            iconIncomplete: "",
-            name: input.achievementName,
-            description : "",
-            gameIcon: "",
-            gameName: "",
-            username: input.username,
-            avatar: input.avatar,
-            steamID: input.steamID,
-            gameID: input.gameID
-        }
-    
-    if(response.status === 200){
-        try{
-        const xml = response.data;
-        let steamProfile = convert.xml2js(xml);
-        let achievements = steamProfile.elements[0].elements[5].elements;
-        
-        achievements.forEach(achievement => {
-            //console.log(achievement.elements[2].elements[0].cdata);
-            //console.log(achievement.elements[2].elements[0].cdata == input.achievementName);
-            if(achievement.elements[2].elements[0].cdata == input.achievementName){
-                //console.log(input.steamID);
-                //console.log("Acheivement found");
-                out.complete = achievement.attributes.closed;
-                out.iconComplete = achievement.elements[0].elements[0].cdata;
-                out.iconIncomplete = achievement.elements[1].elements[0].cdata;
-                out.description = achievement.elements[4].elements[0].cdata;
-                out.gameIcon = steamProfile.elements[0].elements[2].elements[4].elements[0].cdata;
-                out.gameName = steamProfile.elements[0].elements[2].elements[1].elements[0].cdata;
-            }
-        });
-        } catch(error) {
+        complete: 0,
+        iconComplete: "",
+        iconIncomplete: "",
+        name: input.achievementName,
+        description: "",
+        gameIcon: "",
+        gameName: "",
+        username: input.username,
+        avatar: input.avatar,
+        steamID: input.steamID,
+        gameID: input.gameID
+    }
+
+    if (response.status === 200) {
+        try {
+            const xml = response.data;
+            let steamProfile = convert.xml2js(xml);
+            let achievements = steamProfile.elements[0].elements[5].elements;
+
+            achievements.forEach(achievement => {
+                //console.log(achievement.elements[2].elements[0].cdata);
+                //console.log(achievement.elements[2].elements[0].cdata == input.achievementName);
+                if (achievement.elements[2].elements[0].cdata == input.achievementName) {
+                    //console.log(input.steamID);
+                    //console.log("Acheivement found");
+                    out.complete = achievement.attributes.closed;
+                    out.iconComplete = achievement.elements[0].elements[0].cdata;
+                    out.iconIncomplete = achievement.elements[1].elements[0].cdata;
+                    out.description = achievement.elements[4].elements[0].cdata;
+                    out.gameIcon = steamProfile.elements[0].elements[2].elements[4].elements[0].cdata;
+                    out.gameName = steamProfile.elements[0].elements[2].elements[1].elements[0].cdata;
+                }
+            });
+        } catch (error) {
             console.log(`Failed to find achievement for user ${input.username}`);
             console.error(error);
         }
-        if(out.complete == 0)
+        if (out.complete == 0)
             out.complete = false;
-        if(out.complete == 1)
+        if (out.complete == 1)
             out.complete = true;
     } else {
         out.description = "Failed to access steam";
